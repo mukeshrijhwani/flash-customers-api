47184ba445fbfe39bace7d1e1f4c11d08aab0edd
mukeshrijhwani <mukesh.rijhwani@gmail.com>
2018-11-22 15:35:22 +1100

    Add files via upload

--- set_environment_variables.sh
+++ set_environment_variables.sh
@@ -6,19 +6,26 @@ do
   val=$(echo $i | cut -f2 -d=)
   echo " $key and $val "
 
-  if [ "$key" = "GIT_TYPE" ]
+  if test -z "$key"
+  then
+    echo ""
+  elif [[ "$key" =~ ^\# ]]
+  then
+    echo ""
+  elif [ "$key" = "GIT_TYPE" ] 
   then
     GIT_TYPE=$val
-  elif [ "$key" = "GIT_USER" ]
+  elif [ "$key" = "GIT_USER" ] 
   then
     GIT_USER=$val
-  elif [ "$key" = "GIT_PROJECT" ]
+  elif [ "$key" = "GIT_PROJECT" ] 
   then
     GIT_PROJECT=$val
-  elif [ "$key" = "CIRCLECI_TOKEN" ]
+  elif [ "$key" = "CIRCLECI_TOKEN" ] 
   then
     CIRCLECI_TOKEN=$val
   else
-  curl -X POST --header "Content-Type: application/json" -d "{\"name\":\"$key\", \"value\":\"$val\"}" https://circleci.com/api/v1.1/project/$GIT_TYPE/$GIT_USER/$GIT_PROJECT/envvar?circle-token=$CIRCLECI_TOKEN
+  #curl -X POST --header "Content-Type: application/json" -d "{\"name\":\"$key\", \"value\":\"$val\"}" https://circleci.com/api/v1.1/project/$GIT_TYPE/$GIT_USER/$GIT_PROJECT/envvar?circle-token=$CIRCLECI_TOKEN
+  echo "calling" 
   fi
 done
